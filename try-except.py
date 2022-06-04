f97548b07bb939ffb919ef15388c61b396fbd6dc
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-06-04 19:08:02 +0100

    Add files via upload

---
+++ try-except.py
@@ -0,0 +1,7 @@
+try:
+    number = int(input("Please Enter a Number: "))
+    numberone = int(input("Please Enter the Second Number: "))
+    result = number + numberone
+    print(result)
+except:
+    print("Invalid Value")
